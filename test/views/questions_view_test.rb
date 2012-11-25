36f27539aa6db2c23dad72b70b85b15c1bc4bf21
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2012-11-25 02:31:31 -0200

    Tratamento da view questions/index quando não há questões cadastradas

--- test/views/questions_view_test.rb
+++ test/views/questions_view_test.rb
@@ -3,8 +3,18 @@ require 'minitest_helper'
 describe 'Questions View Test' do
   describe "index" do
     describe "with no questions" do
-      it "shows empty message"
-      it "omits table"
+      before do
+        @questions = []
+        render file: "questions/index"
+      end
+
+      it "shows empty message" do
+        assert_select "p.info", "Nenhuma questao cadastrada"
+      end
+
+      it "omits table" do
+        assert_select "table", 0
+      end
     end
 
     describe "with saved questions" do
