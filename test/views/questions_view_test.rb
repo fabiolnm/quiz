fc2582c4e344be834be5c2cd5b0deb8a38dd7b48
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2012-11-25 00:19:10 -0200

    Adiciona spec inicial de Questions View

--- test/views/questions_view_test.rb
+++ test/views/questions_view_test.rb
@@ -1,5 +1,15 @@
 require 'minitest_helper'
 
 describe 'Questions View Test' do
-  it "is a view spec"
+  describe "index" do
+    describe "with no questions" do
+      it "shows empty message"
+      it "omits table"
+    end
+
+    describe "with saved questions" do
+      it "omits empty message"
+      it "shows table"
+    end
+  end
 end
